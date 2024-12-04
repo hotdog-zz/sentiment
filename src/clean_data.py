bb9d6f340274823f6ff5c4452befbcedc4da48bb
hotdog-zz <2761969115@qq.com>
2024-12-04 06:34:53 +0000

    generate classify

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,33 +1,51 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import pandas as pd
+import pdb
+import json
 
-model_name = "Qwen/Qwen2.5-7B-Instruct"
+def go_emotion():
+    emotion_categories = {
+        "positive": ["amusement", "excitement", "joy", "love", "desire", "optimism", "caring", "pride", "admiration", "gratitude", "relief", "approval"],
+        "negative": ["fear", "nervousness", "remorse", "embarrassment", "disappointment", "sadness", "grief", "disgust", "anger", "annoyance", "disapproval"],
+        "ambiguous": ["realization", "surprise", "curiosity", "confusion", "neutral"]
+    }
+    # 读取CSV文件
+    dfs = [pd.read_csv('data/go_emotion/goemotions_1.csv'), pd.read_csv('data/go_emotion/goemotions_2.csv'), pd.read_csv('data/go_emotion/goemotions_3.csv')]
+    df = pd.concat(dfs, ignore_index=True)
 
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    torch_dtype="auto",
-    device_map="auto"
-)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+    result = []
 
-prompt = "Give me a short introduction to large language model."
-messages = [
-    {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
-    {"role": "user", "content": prompt}
-]
-text = tokenizer.apply_chat_template(
-    messages,
-    tokenize=False,
-    add_generation_prompt=True
-)
-model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-
-generated_ids = model.generate(
-    **model_inputs,
-    max_new_tokens=512
-)
-generated_ids = [
-    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-]
-
-response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print(response)
+    # 遍历数据的每一行
+    for idx, row in df.iterrows():
+        # 提取text列
+        text = row['text']
+        
+        # 提取情绪列，筛选出值为1的情绪列
+        emotions = [emotion for emotion in ['admiration', 'amusement', 'anger', 'annoyance', 'approval', 
+                                            'caring', 'confusion', 'curiosity', 'desire', 'disappointment', 
+                                            'disapproval', 'disgust', 'embarrassment', 'excitement', 'fear', 
+                                            'gratitude', 'grief', 'joy', 'love', 'nervousness', 'optimism', 
+                                            'pride', 'realization', 'relief', 'remorse', 'sadness', 'surprise', 
+                                            'neutral'] if row[emotion] == 1]
+        # 分类情绪
+        classify = set()  # 使用集合避免重复分类
+        for emotion in emotions:
+            if emotion in emotion_categories['positive']:
+                classify.add("positive")
+            elif emotion in emotion_categories['negative']:
+                classify.add("negative")
+            elif emotion in emotion_categories['ambiguous']:
+                classify.add("ambiguous")
+        
+        # 将分类结果转换为列表
+        classify_list = list(classify)
+        # 构建字典并添加到结果列表
+        result.append({
+            'idx': idx,
+            'dataset': 'go_emotion',
+            'text': text,
+            'classify': classify_list,
+            'emotion': emotions
+        })
+    with open("data/go_emotion/clean_data.json", "w") as f:
+        json.dump(result, f, indent=2)
+go_emotion()
---
+++ src/llm_data.py
@@ -0,0 +1,155 @@
+import argparse
+import json
+import os
+import re
+import pdb
+from openai import OpenAI
+from datasets import load_dataset
+from tqdm import tqdm
+from vllm import LLM, SamplingParams
+import multiprocessing
+import time
+import random
+
+
+def arg_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="Qwen/Qwen2.5-7B-Instruct",
+    )
+    parser.add_argument(
+        "--actor_name",
+        type=str,
+        default="Qwen/Qwen2.5-7B-Instruct",
+    )
+    parser.add_argument(
+        "--dataset_name",
+        nargs='+',
+        default=["go_emotion"],
+    )
+    parser.add_argument("--temperature", type=float, default=0.7)
+    parser.add_argument("--sample_num", type=int, default=1)
+    parser.add_argument("--results_file", type=str, default="llm_data/temp.json")
+    parser.add_argument("--mode", type=str, default="inference")
+    args = parser.parse_args()
+    return args
+
+def clean_response(response):
+    classify = []
+    temp = response.find("\n")
+    response = response[0:temp]
+    if "negative" in response:
+        classify.append("negative")
+    if "positive" in response:
+        classify.append("positive")
+    if "ambiguous" in response:
+        classify.append("ambiguous")
+    return classify
+
+def generate_classify_template(text, model_name):
+    if model_name == "Qwen/Qwen2.5-7B-Instruct":
+        prompt = f"<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.<|im_end|>\n<|im_start|>user\nPlease read the following sentence and classify it into one of the three categories based on the emotion expressed: negative, ambiguous, or positive.\n\nYour answer format should be:\nAfter analyzing the whole sentence, I will classify it into [negative/ambiguous/positive].\n\nSentence:\n{text}<|im_end|>\n<|im_start|>assistant\nAfter analyzing the whole sentence, I will classify it into "
+    return prompt
+
+
+def generate_responses(model, sampling_params, prompt):
+    outputs = model.generate(prompt, sampling_params)
+    responses = [output.outputs[0].text for output in outputs]
+    return responses
+
+def batch_inference(
+    inference_dataset,
+    device_id,
+    sampling_params,
+    batch_size,
+    actor_name,
+    model_name,
+):
+    # device_id = device_id % 8
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(device_id)
+    model = LLM(model=actor_name, tensor_parallel_size=1, gpu_memory_utilization=0.85, max_model_len=2048)
+    total_batches = (len(inference_dataset) + batch_size - 1) // batch_size
+    results_true = []
+    
+    # Batch inference
+    for batch_idx in tqdm(range(total_batches)):
+        start_index = batch_idx * batch_size
+        end_index = min((batch_idx + 1) * batch_size, len(inference_dataset))
+        batch_items = inference_dataset[start_index: end_index]
+
+        prompts = [
+            generate_classify_template(item['text'], model_name)
+            for item in batch_items
+        ]
+        # Generate responses using llama
+        batch_responses = generate_responses(
+            model=model,
+            sampling_params=sampling_params,
+            prompt=prompts,
+        )
+        for idx, data in enumerate(batch_items):
+            response = batch_responses[idx]
+            if response[0] == " ":
+                response = response[1:]
+            new_data = {}
+            new_data["idx"] = data["idx"]
+            new_data["dataset"] = data["dataset"]
+            new_data["text"] = data["text"]
+            new_data["classify"] = data["classify"]
+            new_data["emotion"] = data["emotion"]
+            new_data["actor_model"] = actor_name
+            new_data["classify_response"] = "After analyzing the whole sentence, I will classify it into " + response
+            new_data["origin_classify"] = clean_response(response)
+            results_true.append(new_data)
+    return results_true
+
+
+def inference_pipeline(
+    actor_name,
+    model_name,
+    ds, 
+    temperature, 
+    sample_num, 
+    results_path,
+):
+    gpu_id = [1, 4, 5, 6]
+    total_gpu = len(gpu_id)
+    sampling_params = SamplingParams(max_tokens=512, temperature=temperature)
+    # random.shuffle(ds)
+    ds = [value for value in ds for i in range(sample_num)]
+    inference_dataset = []
+    batch_size = (len(ds) - 1) // total_gpu + 1
+    for batch_idx in range(total_gpu):
+        start_index = batch_idx * batch_size
+        end_index = min((batch_idx + 1) * batch_size, len(ds))
+        inference_dataset.append(ds[start_index: end_index])
+    true_data = []
+    false_data = []
+
+    with multiprocessing.Pool(processes=total_gpu) as pool:
+        results = [
+            pool.apply_async(batch_inference, args=(inference_dataset[device_id], gpu_id[device_id], sampling_params, 256, actor_name, model_name))
+            for device_id in range(total_gpu)
+        ]
+        for r in results:
+            result_true = r.get()
+            true_data += result_true
+    with open(results_path, "w") as g:
+        json.dump(true_data, g, indent=2)
+
+
+if __name__ == "__main__":
+    args = arg_parse()
+    actor_name = args.actor_name
+    model_name = args.model_name
+    dataset_name = args.dataset_name
+    temperature = args.temperature
+    sample_num = args.sample_num
+    mode = args.mode
+    for ds_name in dataset_name:
+        with open(f"data/{ds_name}/clean_data.json", "r") as f:
+            ds = json.load(f)
+        results_path = f"llm_data/{ds_name}.json"
+        inference_pipeline(actor_name, model_name, ds, temperature, sample_num, results_path)
