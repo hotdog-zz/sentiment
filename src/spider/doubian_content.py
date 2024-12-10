ea338db5150d0a635d90a6e6cf8f1d84b51e3f0d
hotdog-zz <2761969115@qq.com>
2024-12-10 12:41:55 +0000

    douban

--- src/spider/doubian_content.py
+++ src/spider/doubian_content.py
@@ -26,7 +26,8 @@ def get_topic_info(url_list):
         columns=['url', 'user', 'user_url', 'title', 'content', 'time', 'react_num', 'collect_num', 'timestamp'])
     comment_df = pd.DataFrame(columns=[
         'url', 'comment_id', 'user', 'user_url', 'comment', 'time', 'reply_to', 'timestamp'])
-    flag = False
+    flag = True
+
     for url in url_list:
         if "topic" in url:
             if "307423713" in url:
@@ -38,7 +39,7 @@ def get_topic_info(url_list):
                         soup = BeautifulSoup(response.text, 'lxml')
                         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                         print("url:%s, Time:%s" % (url, timestamp))
-                        
+
                         # 顶楼部分
                         content_raw = soup.find('div', class_='topic-doc')
                         if "有异常请求" in str(soup):
@@ -61,7 +62,7 @@ def get_topic_info(url_list):
                         content_col = {'url': url, 'user': user, 'user_url': user_url, 'title': title, 'content': content,
                                     'time': time, 'timestamp': timestamp}
                         # print(content_col)
-                        content_df = content_df.append(content_col, ignore_index=True)
+                        content_df = pd.concat([content_df, pd.DataFrame([content_col])], ignore_index=True)
 
                         # 评论部分
                         comment_raw = soup.find('ul', id='comments')
@@ -103,9 +104,9 @@ def get_topic_info(url_list):
                                 reply_to = com.find(
                                     'div', class_='reply-quote-content').get('data-ref-cid')
                             comment_col = {'url': url, 'comment_id': comment_id, 'user': user, 'user_url': user_url,
-                                        'time': time, 'comment': comment, 'reply_to': reply_to, 'my_timestamp': timestamp}
+                                        'time': time, 'comment': comment, 'reply_to': reply_to, 'timestamp': timestamp}
                             # print(comment_col)
-                            comment_df = comment_df.append(comment_col, ignore_index=True)
+                            comment_df = pd.concat([comment_df, pd.DataFrame([comment_col])], ignore_index=True)
                         break
                     except Exception as e:
                         print(e)
@@ -116,14 +117,116 @@ def get_topic_info(url_list):
                             index=False, encoding='utf-8-sig')
                 comment_df.to_csv('discussion_reply.csv',
                                 index=False, encoding='utf-8-sig')
+        elif "people" in url:
+            if "307423713" in url:
+                flag = True
+            if flag:
+                for _ in range(50):
+                    try:
+                        response = requests.get(url, headers=header)
+                        sleep(30)
+                        soup = BeautifulSoup(response.text, 'lxml')
+                        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                        print("url:%s, Time:%s" % (url, timestamp))
+
+                        content_raw = soup.find("div", class_='status-item')
+                        if content_raw is None:
+                            print('url:%s, content_raw is None' % url)
+                            break
+                        if content_raw is not None:
+                            content = content_raw.find('div', class_='status-title')
+                            if content is not None:
+                                title = content.get_text(strip=True)
+                            else:
+                                title = ""
+
+                            content = content_raw.find("p")
+                            if content is not None:
+                                content = content.get_text(strip=True)
+                            else:
+                                content = ""
+
+                            meta_data_raw = content_raw.find('div', class_='text')
+                            try:
+                                user_url = meta_data_raw.find('a', class_='lnk-people')['href']
+                                user = meta_data_raw.find('a', class_='lnk-people').get_text(strip=True)
+                                time = meta_data_raw.find('span', class_='pubtime').get_text(strip=True)
+                            except Exception as e:
+                                print(e)
+                                print('url:%s, something is None' % url)
+                                break
+
+                            content_col = {'url': url, 'user': user, 'user_url': user_url, 'title': title, 'content': content,
+                                    'time': time, 'timestamp': timestamp}
+                            content_df = pd.concat([content_df, pd.DataFrame([content_col])], ignore_index=True)
+
+                        comment_raw = soup.find('script', text=re.compile(r'var _COMMENTS_CONFIG'))
+                        if comment_raw is not None:
+                            num = 0
+                            while True:
+                                comment_all_list = comment_raw.string
+                                comment_begin = comment_all_list.find("'comments':")
+                                comment_end = comment_all_list.find("'total':")
+                                total_end = comment_all_list.find("'start'")
+
+                                comment_all = comment_all_list[comment_begin: comment_end]
+                                total_num = comment_all_list[comment_end: total_end]
+                                total_num = int(re.search(r"'total': (\d+)", total_num).group(1))
+
+                                comment_list = json.loads(comment_all[12:-7])
+                                for comments in comment_list:
+                                    comment_id = comments["id"]
+                                    user = comments["author"]["name"]
+                                    user_url = comments["author"]["url"]
+                                    time = comments["create_time"]
+                                    comment = comments["text"]
+                                    reply_to = "" if comments["parent_comment_id"] == '0' else comments["parent_comment_id"]
+                                    comment_col = {'url': url, 'comment_id': comment_id, 'user': user, 'user_url': user_url,
+                                            'time': time, 'comment': comment, 'reply_to': reply_to, 'timestamp': timestamp}
+                                    
+                                    # print(comment_col)
+                                    comment_df = pd.concat([comment_df, pd.DataFrame([comment_col])], ignore_index=True)
+
+                                    if len(comments['replies']) != 0:
+                                        for replies in comments['replies']:
+                                            comment_id = replies["id"]
+                                            user = replies["author"]["name"]
+                                            user_url = replies["author"]["url"]
+                                            time = replies["create_time"]
+                                            comment = replies["text"]
+                                            reply_to = replies['ref_comment']['id']
+                                            comment_col = {'url': url, 'comment_id': comment_id, 'user': user, 'user_url': user_url,
+                                                    'time': time, 'comment': comment, 'reply_to': reply_to, 'timestamp': timestamp}
+                                            
+                                            # print(comment_col)
+                                            comment_df = pd.concat([comment_df, pd.DataFrame([comment_col])], ignore_index=True)
+                                    
+                                num += 20
+                                if num >= total_num:
+                                    break
+                                else:
+                                    response = requests.get(url + f"/?start={num}", headers=header)
+                                    sleep(30)
+                                    soup = BeautifulSoup(response.text, 'lxml')
+                                    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                                    print("url:%s, Time:%s" % (url, timestamp))
+
+                                    comment_raw = soup.find('script', text=re.compile(r'var _COMMENTS_CONFIG'))
+                                    if comment_raw is None:
+                                        break
+                        content_df.to_csv('discussion_content.csv',
+                                    index=False, encoding='utf-8-sig')
+                        comment_df.to_csv('discussion_reply.csv',
+                                    index=False, encoding='utf-8-sig')
+                        break
+                    except Exception as e:
+                        print(e)
+                        sleep(30)
         
     return content_df, comment_df
 
 if __name__ == '__main__':
-    '''
-    调用get_discussion_list函数获取url_list，
-    或读取已经保存到本地的文件。
-    '''
+
     with open("url_姜萍.json", "r") as f:
         url_list = json.load(f)
     with open("url_数学竞赛中专.json", "r") as f:
